35d97474589171bafd80876b3a72ffb00f64fe20
520-forever <a441832620@126.com>
2021-10-23 21:43:00 +0800

    news

--- React/todolist/src/App.js
+++ React/todolist/src/App.js
@@ -1,9 +1,11 @@
-function App() {
-  return (
-    <div className="App">
-   Hello I am dell
-    </div>
-  );
+import React, { Component } from "react";
+class App extends Component{
+  render(){
+    return(
+      <div>
+        Hello I am dell
+      </div>
+    )
+  }
 }
-
 export default App;
