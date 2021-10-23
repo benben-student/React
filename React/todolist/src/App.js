2a8003b8eaf3bae41016789271050e9549bf6c3c
520-forever <a441832620@126.com>
2021-10-23 23:04:25 +0800

    new

--- React/todolist/src/App.js
+++ React/todolist/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 //import './App.css';
+import Top from "./Todo";
 class App extends Component{
   render(){
     return(
       <div>
+        <Top />
         Hello I am dell
       </div>
     )
---
+++ React/todolist/src/Todo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class Top extends React.Component {
+    render() {
+        return (
+            <div>Top</div>
+        )
+    }
+}
+
+export default Top;
